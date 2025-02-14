4f76b562713f111d382af607a71cf7bedbad558b
sandhya8927 <159907923+sandhya8927@users.noreply.github.com>
2025-02-14 08:49:47 +0530

    Add files via upload

---
+++ EX-8.1.R
@@ -0,0 +1,13 @@
+sink()
+# Extract the first 10 English letters in lower case
+first_10_lower <- letters[1:10]
+print(first_10_lower)
+
+# Extract the last 10 English letters in upper case
+last_10_upper <- LETTERS[17:26]
+print(last_10_upper)
+
+# Extract letters between the 22nd to 24th letters in upper case
+letters_22_24_upper <- LETTERS[22:24]
+print(letters_22_24_upper)
+sink()
