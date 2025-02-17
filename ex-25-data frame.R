698636228bb9ab824ede045475eb514fdd15359d
sandhya8927 <159907923+sandhya8927@users.noreply.github.com>
2025-02-17 11:44:17 +0530

    Add files via upload

---
+++ ex-25-data frame.R
@@ -0,0 +1,20 @@
+# Create the data frame
+sink("ex-25-data frame.txt")
+exam_data <- data.frame(
+  name = c('Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'),
+  score = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19),
+  attempts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1),
+  qualify = c('yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes')
+)
+
+# a. Extract 3rd and 5th rows with 1st and 3rd columns
+extracted_data <- exam_data[c(3, 5), c(1, 3)]
+print("Extracted 3rd and 5th rows with 1st and 3rd columns:")
+print(extracted_data)
+
+# b. Add a new column named 'country'
+country <- c("USA", "USA", "USA", "USA", "UK", "USA", "USA", "India", "USA", "USA")
+exam_data$country <- country
+print("Data frame with new column 'country':")
+print(exam_data)
+sink("file=null")̥
